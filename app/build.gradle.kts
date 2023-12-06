17ea113193c4bc7ddf3517cb41db3e55f1767e50
NguyenTanLap <135099342+tannguyennn@users.noreply.github.com>
2023-12-06 14:24:17 +0700

    upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.nguyentanlap.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.nguyentanlap.myapplication"
